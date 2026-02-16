6951d5721dce5bb99cc8ac56f7d712dbe30040f1
uduladapsara <dapsaraudula@gmail.com>
2026-02-16 13:17:02 +0530

    Promises-udula

---
+++ Async.js
@@ -0,0 +1,10 @@
+async function myFunction() { 
+  try { 
+    const result = await myPromise; 
+    console.log(result); 
+  } catch (error) { 
+    console.log(error); 
+  } 
+} 
+ 
+myFunction();
