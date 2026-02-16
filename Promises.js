e6766ee2f874a5fd1e24cd44fa0eb53259e13d66
uduladapsara <dapsaraudula@gmail.com>
2026-02-16 13:14:19 +0530

    Promises-udula

---
+++ Promises.js
@@ -0,0 +1,13 @@
+const myPromise = new Promise((resolve, reject) => { 
+  if (condition) { 
+    resolve('Success!'); 
+  } else { 
+    reject('Failure!'); 
+  } 
+}); 
+ 
+myPromise.then((result) => { 
+  console.log(result); 
+}).catch((error) => { 
+  console.log(error); 
+}); 
